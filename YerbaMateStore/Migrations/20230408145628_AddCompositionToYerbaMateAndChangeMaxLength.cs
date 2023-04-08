032e211347706614d6ad1ca7b6be1e5f7c06d4c8
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-08 16:59:08 +0200

    Refactor YerbaMate Entity & DB table

---
+++ YerbaMateStore/Migrations/20230408145628_AddCompositionToYerbaMateAndChangeMaxLength.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YerbaMateStore.Migrations
+{
+    public partial class AddCompositionToYerbaMateAndChangeMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "YerbaMate",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldMaxLength: 30)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Composition",
+                table: "YerbaMate",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "YerbaMate",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)",
+                oldMaxLength: 30)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "YerbaMate",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Composition",
+                table: "YerbaMate",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "YerbaMate",
+                type: "varchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- YerbaMateStore/Models/Entities/Configurations/YerbaMateConfiguration.cs
+++ YerbaMateStore/Models/Entities/Configurations/YerbaMateConfiguration.cs
@@ -7,10 +7,11 @@ public class YerbaMateConfiguration : IEntityTypeConfiguration<YerbaMate>
 {
   public void Configure(EntityTypeBuilder<YerbaMate> builder)
   {
-    builder.Property(p => p.Name).IsRequired().HasMaxLength(30);
-    builder.Property(p => p.Brand).IsRequired().HasMaxLength(30);
+    builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+    builder.Property(p => p.Brand).IsRequired().HasMaxLength(50);
     builder.Property(p => p.Description).IsRequired().HasMaxLength(300);
     builder.Property(p => p.HasAdditives).IsRequired();
+    builder.Property(p => p.Composition).IsRequired().HasMaxLength(50);
     builder.Property(p => p.Weight).IsRequired().HasMaxLength(10);
     builder.Property(p => p.Price).IsRequired().HasPrecision(10, 2);
     builder.Property(p => p.DiscountPrice).HasPrecision(10, 2);
