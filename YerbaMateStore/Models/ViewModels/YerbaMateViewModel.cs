35810d6810c8e3bacc489e6213a1a0d5d077637a
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-08 15:17:30 +0200

    create YerbaMateVIewModel

---
+++ YerbaMateStore/Models/ViewModels/YerbaMateViewModel.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YerbaMateStore.Models.DataAccess;
+using YerbaMateStore.Models.Entities;
+using YerbaMateStore.Models.Repository.IRepository;
+
+namespace YerbaMateStore.Models.ViewModels;
+
+public class YerbaMateViewModel
+{
+  private readonly IUnitOfWork _unitOfWork;
+  public YerbaMate YerbaMate { get; set; }
+
+  [ValidateNever]
+  public IEnumerable<SelectListItem> CountryList { get; set; }
+
+  public YerbaMateViewModel(IUnitOfWork unitOfWork, YerbaMate? product = null, int? id = null)
+  {
+    _unitOfWork = unitOfWork;
+    this.CountryList = _unitOfWork.Countries.GetAll().Select(c => new SelectListItem
+    {
+      Text = c.Name,
+      Value = c.Id.ToString(),
+    });
+    if (id == null || id == 0)
+    {
+      this.YerbaMate = product == null ? new YerbaMate() : product;
+    }
+    else
+    {
+      this.YerbaMate = _unitOfWork.YerbaMate.GetFirstOrDefault(p => p.Id == id);
+    }
+  }
+}
