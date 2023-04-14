60d61532d41e8b09918f24fcb4b100f9f033215e
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-14 10:34:32 +0200

    Refactor ProductManager

--- YerbaMateStore/Models/Managers/ProductManager.cs
+++ YerbaMateStore/Models/Managers/ProductManager.cs
@@ -7,52 +7,42 @@ public class ProductManager<T> where T : class, new()
 {
   private readonly string wwwRootPath;
   private ProductViewModel<T>? ProductVM { get; set; }
+  private char separationChar;
+  private string productDirectoryName;
 
   public ProductManager(IWebHostEnvironment hostEnvironment, ProductViewModel<T>? productVM = null)
   {
     wwwRootPath = hostEnvironment.WebRootPath;
     ProductVM = productVM;
+    separationChar = Path.DirectorySeparatorChar;
+    productDirectoryName = typeof(T).Name;
   }
 
   public void SaveFile(ref Image<T> productImage, IFormFile file)
   {
     string extension = Path.GetExtension(file.FileName);
-    string fileName = Guid.NewGuid().ToString();
-    char separation = Path.DirectorySeparatorChar;
-    var productDirectoryName = typeof(T).Name;
-    var Id = ProductVM.Product.GetType().GetProperty("Id").GetValue(ProductVM.Product);
-    string productPath = @$"images{separation}products{separation}{productDirectoryName}-{Id}";
+    string fileName = Guid.NewGuid().ToString() + extension;
+    string? Id = ProductVM.Product.GetType().GetProperty("Id").GetValue(ProductVM.Product).ToString();
+    string productPath = @$"images{separationChar}products{separationChar}{productDirectoryName}-{Id}";
     string finalPath = Path.Combine(wwwRootPath, productPath);
-    if (!Directory.Exists(finalPath))
-      Directory.CreateDirectory(finalPath);
 
-    using (var fileStream = new FileStream(Path.Combine(finalPath, fileName + extension), FileMode.Create))
+    if (!Directory.Exists(finalPath))
+    {
+      Directory.CreateDirectory(finalPath);
+    }
+
+    using (var fileStream = new FileStream(Path.Combine(finalPath, fileName), FileMode.Create))
     {
       file.CopyTo(fileStream);
     }
 
     productImage.ProductId = Convert.ToInt32(Id);
-    productImage.ImageUrl = @$"{separation}{productPath}{separation}{fileName}{extension}";
-  }
-
-  public void DeleteFile(Image<T> imageToDelete)
-  {
-    if (!string.IsNullOrEmpty(imageToDelete.ImageUrl))
-    {
-      string productImageDbPath = imageToDelete.ImageUrl.Trim('/');
-      string imagePath = Path.Combine(wwwRootPath, productImageDbPath);
-      if (System.IO.File.Exists(imagePath))
-      {
-        System.IO.File.Delete(imagePath);
-      }
-    }
+    productImage.ImageUrl = @$"{separationChar}{productPath}{separationChar}{fileName}";
   }
 
   public void DeleteFiles(int? id)
   {
-    char separation = Path.DirectorySeparatorChar;
-    string productDirectoryName = typeof(T).Name;
-    string productPath = @$"images{separation}products{separation}{productDirectoryName}-{id}";
+    string productPath = @$"images{separationChar}products{separationChar}{productDirectoryName}-{id}";
     string finalPath = Path.Combine(wwwRootPath, productPath);
     if (Directory.Exists(finalPath))
     {
@@ -65,5 +55,17 @@ public class ProductManager<T> where T : class, new()
     }
   }
 
+  public void DeleteFile(Image<T> imageToDelete)
+  {
+    if (!string.IsNullOrEmpty(imageToDelete.ImageUrl))
+    {
+      string productImageDbPath = imageToDelete.ImageUrl.Trim(separationChar);
+      string imagePath = Path.Combine(wwwRootPath, productImageDbPath);
+      if (System.IO.File.Exists(imagePath))
+      {
+        System.IO.File.Delete(imagePath);
+      }
+    }
+  }
 }
 
