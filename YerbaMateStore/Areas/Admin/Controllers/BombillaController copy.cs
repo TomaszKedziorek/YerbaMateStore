1b9190377ccf436be00372c28b96390e380ea63e
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-14 12:32:03 +0200

    Create BombillaController & View

---
+++ YerbaMateStore/Areas/Admin/Controllers/BombillaController copy.cs
@@ -0,0 +1,124 @@
+using Microsoft.AspNetCore.Mvc;
+using YerbaMateStore.Models.Entities;
+using YerbaMateStore.Models.Managers;
+using YerbaMateStore.Models.Repository.IRepository;
+using YerbaMateStore.Models.ViewModels;
+
+namespace YerbaMateStore.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class BombillaController : Controller
+{
+  private readonly IUnitOfWork _unitOfWork;
+  private readonly IWebHostEnvironment _hostEnvironment;
+
+  public BombillaController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+  {
+    _unitOfWork = unitOfWork;
+    _hostEnvironment = hostEnvironment;
+  }
+
+  [HttpGet]
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  [HttpGet]
+  public IActionResult Upsert(int? id)
+  {
+    ProductViewModel<Bombilla> productVM = new ProductViewModel<Bombilla>(_unitOfWork, null, id);
+    return View(productVM);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public IActionResult Upsert(ProductViewModel<Bombilla> productVM, List<IFormFile> files)
+  {
+    if (productVM.Product.Id == 0 && (files.Count == 0 || files == null))
+    {
+      ModelState.AddModelError("Product.Images", "At least one picture is required.");
+    }
+    if (ModelState.IsValid)
+    {
+      if (productVM.Product.Id == 0)
+      {
+        _unitOfWork.Bombilla.Add(productVM.Product);
+      }
+      else
+      {
+        _unitOfWork.Bombilla.Update(productVM.Product);
+      }
+      _unitOfWork.Save();
+
+      if (files != null)
+      {
+        ProductManager<Bombilla> productManager = new(_hostEnvironment, productVM);
+        if (productVM.Product.Images == null)
+        {
+          productVM.Product.Images = new List<BombillaImage>();
+        }
+        foreach (IFormFile file in files)
+        {
+          Image<Bombilla> productImage = new BombillaImage();
+          productManager.SaveFile(ref productImage, file);
+          productVM.Product.Images.Add((BombillaImage)productImage);
+        }
+        _unitOfWork.Bombilla.Update(productVM.Product);
+        _unitOfWork.Save();
+      }
+      TempData["success"] = "Product updated successfully!";
+      return RedirectToAction(nameof(Index));
+    }
+    else
+    {
+      productVM = new ProductViewModel<Bombilla>(_unitOfWork, productVM.Product);
+      TempData["error"] = "Product updated failed!";
+      return View(productVM);
+    }
+  }
+
+  [HttpGet]
+  public IActionResult DeleteImage(int imageId)
+  {
+    var imageToDelete = _unitOfWork.BombillaImage.GetFirstOrDefault(i => i.Id == imageId);
+    int productId = imageToDelete.ProductId;
+    if (imageToDelete != null)
+    {
+      ProductManager<Bombilla> productManager = new(_hostEnvironment);
+      productManager.DeleteFile(imageToDelete);
+      _unitOfWork.BombillaImage.Remove(imageToDelete);
+      _unitOfWork.Save();
+      TempData["success"] = "Deleted succesfully!";
+    }
+    return RedirectToAction(nameof(Upsert), new { id = productId });
+  }
+
+
+  //API CALLS
+  [HttpGet]
+  public IActionResult GetAll()
+  {
+    IEnumerable<Bombilla>? productList = _unitOfWork.Bombilla.GetAll();
+    return Json(new { data = productList });
+  }
+
+  [HttpDelete]
+  public IActionResult Delete(int? id)
+  {
+    Bombilla? product = _unitOfWork.Bombilla.GetFirstOrDefault(p => p.Id == id);
+
+    if (product == null)
+    {
+      return Json(new { success = false, message = "Error while deleting!" });
+    }
+    else
+    {
+      ProductManager<Bombilla> productManager = new(_hostEnvironment);
+      productManager.DeleteFiles(id);
+      _unitOfWork.Bombilla.Remove(product);
+      _unitOfWork.Save();
+      return Json(new { success = true, message = "Delete Successful!" });
+    }
+  }
+}
---
+++ YerbaMateStore/wwwroot/js/bombillaProduct.js
@@ -0,0 +1,47 @@
+let dataTable;
+
+$(document).ready(function () {
+  loadDataTable();
+})
+
+function loadDataTable() {
+  dataTable = $('#bombillaProductData').DataTable({
+    ajax: {
+      url: '/Admin/Bombilla/GetAll'
+    },
+    responsive: true,
+    columns: [
+      { "data": "id", "width": "5%" },
+      { "data": "name", "width": "15%" },
+      { "data": "material", "width": "10%" },
+      { "data": "length", "width": "10%" },
+      { "data": "isUnscrewed", "width": "5%" },
+      {
+        "data": "price", "width": "10%", "render":
+          function (data) {
+            return data + " zł";
+          }
+      },
+      {
+        "data": "id",
+        "width": "15%",
+        "render": function (data) {
+          return `
+          <div class="row justify-content-center">
+            <div class="col-6 pe-1">
+              <a href="/Admin/Bombilla/Upsert?id=${data}" class="btn btn-primary w-100 px-1">
+                <i class="bi bi-pencil-square"></i><span class="d-none d-md-block">Edit</span>
+              </a>
+            </div>
+            <div class="col-6 ps-1">
+              <a onClick="Delete('/Admin/Bombilla/Delete/${data}')" class="btn btn-danger w-100 px-1">
+                <i class="bi bi-x-circle"></i><span class="d-none d-md-block">Delete</span>
+              </a>
+            </div>
+          </div>
+        `
+        }
+      }
+    ]
+  });
+}
