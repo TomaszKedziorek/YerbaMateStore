057362b036f95ddd11b72b0676b94635e8d766ba
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-18 14:07:58 +0200

    Add filters to YerbaMateVM

--- YerbaMateStore/Areas/Customer/Controllers/YerbaMateController.cs
+++ YerbaMateStore/Areas/Customer/Controllers/YerbaMateController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Linq.Expressions;
 using Microsoft.AspNetCore.Mvc;
 using YerbaMateStore.Models.Entities;
 using YerbaMateStore.Models.Repository.IRepository;
@@ -16,11 +17,43 @@ public class YerbaMateController : Controller
   }
 
   [HttpGet]
-  public IActionResult Index()
+  public IActionResult Index(string type, string country, string brand, string weight)
   {
-    IEnumerable<YerbaMate> productList = _unitOfWork.YerbaMate.GetAll(includeProperties: "Images");
-    return View(productList);
+    YerbaMateProductsViewModel YerbaMateVM = new(_unitOfWork, type, country, brand, weight);
+    return View(YerbaMateVM);
   }
+  // [HttpGet]
+  // public IActionResult Index(string condition)
+  // {
+  //   IEnumerable<YerbaMate> productList = GetAllWhere(condition);
+  //   if (productList == null)
+  //   {
+  //     productList = _unitOfWork.YerbaMate.GetAll(includeProperties: "Images");
+  //   }
+  //   return View(productList);
+  // }
+
+  // private IEnumerable<YerbaMate> GetAllWhere(string condition)
+  // {
+  //   Expression<Func<YerbaMate, bool>> filter;
+  //   switch (condition)
+  //   {
+  //     case "all":
+  //       filter = p => p.Id != 0;
+  //       break;
+  //     case "classic":
+  //       filter = p => p.HasAdditives == false;
+  //       break;
+  //     case "additives":
+  //       filter = p => p.HasAdditives == true;
+  //       break;
+  //     default:
+  //       filter = p => p.Id != 0;
+  //       break;
+  //   }
+  //   IEnumerable<YerbaMate> productList = _unitOfWork.YerbaMate.GetAll(filter, includeProperties: "Images");
+  //   return productList;
+  // }
 
 
   public IActionResult Privacy()
---
+++ YerbaMateStore/Models/ViewModels/YerbaMateProductsViewModel.cs
@@ -0,0 +1,83 @@
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using YerbaMateStore.Models.Repository.IRepository;
+using YerbaMateStore.Models.Entities;
+
+namespace YerbaMateStore.Models.ViewModels;
+
+public class YerbaMateProductsViewModel
+{
+  private readonly IUnitOfWork _unitOfWork;
+  public IEnumerable<YerbaMate> ProductList { get; set; }
+
+  public IEnumerable<Country> CountryList { get; set; }
+  public IEnumerable<String> BrandList { get; set; }
+  public IEnumerable<String> WeigthList { get; set; }
+  public string? Filter { get; set; }
+
+  public YerbaMateProductsViewModel(IUnitOfWork unitOfWork,
+    string? type = null, string? country = null, string? brand = null, string? weight = null)
+  {
+    _unitOfWork = unitOfWork;
+    this.CountryList = _unitOfWork.Countries.GetAll();
+    this.BrandList = _unitOfWork.YerbaMate.GetAll().Select(p => p.Brand).Distinct();
+    this.WeigthList = _unitOfWork.YerbaMate.GetAll().Select(p => p.Weight).Distinct();
+    this.ProductList = GetAllWhere(type, country, brand, weight);
+  }
+  private Expression<Func<YerbaMate, bool>> TypeFilter(string type)
+  {
+    Expression<Func<YerbaMate, bool>> filter;
+    switch (type)
+    {
+      case "all":
+        filter = p => p.Id != 0;
+        this.Filter = "All";
+        break;
+      case "classic":
+        filter = p => p.HasAdditives == false;
+        this.Filter = "Classic";
+        break;
+      case "additives":
+        filter = p => p.HasAdditives == true;
+        this.Filter = "With Additives";
+        break;
+      default:
+        filter = p => p.Id != 0;
+        this.Filter = "All";
+        break;
+    }
+    return filter;
+  }
+
+  private Expression<Func<YerbaMate, bool>> CountryFilter(string country)
+  {
+    Expression<Func<YerbaMate, bool>> filter = p => p.Country.Name.Contains(country);
+    this.Filter = country;
+    return filter;
+  }
+  private Expression<Func<YerbaMate, bool>> BrandFilter(string brand)
+  {
+    Expression<Func<YerbaMate, bool>> filter = p => p.Brand.Contains(brand);
+    this.Filter = brand;
+    return filter;
+  }
+  private Expression<Func<YerbaMate, bool>> WeightFilter(string weight)
+  {
+    Expression<Func<YerbaMate, bool>> filter = p => p.Weight.Contains(weight);
+    this.Filter = weight;
+    return filter;
+  }
+
+  private IEnumerable<YerbaMate> GetAllWhere(string? type = null, string? country = null, string? brand = null, string? weight = null)
+  {
+    Expression<Func<YerbaMate, bool>> filter = null;
+    if (!string.IsNullOrEmpty(type)) { filter = TypeFilter(type); }
+    if (!string.IsNullOrEmpty(country)) { filter = CountryFilter(country); }
+    if (!string.IsNullOrEmpty(brand)) { filter = BrandFilter(brand); }
+    if (!string.IsNullOrEmpty(weight)) { filter = WeightFilter(weight); }
+    if (string.IsNullOrEmpty(this.Filter)) { this.Filter = "All"; }
+    IEnumerable<YerbaMate> productList = _unitOfWork.YerbaMate.GetAll(filter, includeProperties: "Images,Country");
+    return productList;
+  }
+}
