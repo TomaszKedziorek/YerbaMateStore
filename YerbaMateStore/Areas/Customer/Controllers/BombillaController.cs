c1122f144726270262b49301e15dcc51f36149b2
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-19 17:34:08 +0200

    Add Bombilla Products $ product Details

---
+++ YerbaMateStore/Areas/Customer/Controllers/BombillaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YerbaMateStore.Models.Entities;
+using YerbaMateStore.Models.Repository.IRepository;
+using YerbaMateStore.Models.ViewModels;
+
+namespace YerbaMateStore.Controllers;
+[Area("Customer")]
+public class BombillaController : Controller
+{
+  private readonly IUnitOfWork _unitOfWork;
+
+  public BombillaController(IUnitOfWork unitOfWork)
+  {
+    _unitOfWork = unitOfWork;
+  }
+
+  [HttpGet]
+  public IActionResult Index(string type, string length, string material)
+  {
+    BombillaProductsViewModel BombillaVM = new(_unitOfWork, type, length, material);
+    return View(BombillaVM);
+  }
+
+  [HttpGet]
+  public IActionResult Details(int productId)
+  {
+    ShopProductViewModel<Bombilla> productVM = new ShopProductViewModel<Bombilla>(_unitOfWork, productId);
+    return View(productVM);
+  }
+
+}
