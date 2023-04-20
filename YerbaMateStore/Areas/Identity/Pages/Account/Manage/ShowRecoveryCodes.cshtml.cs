0c9cb1545d3a0384255f85e5f34d420b898b48a8
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-20 13:15:09 +0200

    format all Identity files

--- YerbaMateStore/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
+++ YerbaMateStore/Areas/Identity/Pages/Account/Manage/ShowRecoveryCodes.cshtml.cs
@@ -9,38 +9,38 @@ using Microsoft.Extensions.Logging;
 
 namespace YerbaMateStore.Areas.Identity.Pages.Account.Manage
 {
+  /// <summary>
+  ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+  ///     directly from your code. This API may change or be removed in future releases.
+  /// </summary>
+  public class ShowRecoveryCodesModel : PageModel
+  {
     /// <summary>
     ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
     ///     directly from your code. This API may change or be removed in future releases.
     /// </summary>
-    public class ShowRecoveryCodesModel : PageModel
+    [TempData]
+    public string[] RecoveryCodes { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    [TempData]
+    public string StatusMessage { get; set; }
+
+    /// <summary>
+    ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+    ///     directly from your code. This API may change or be removed in future releases.
+    /// </summary>
+    public IActionResult OnGet()
     {
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string[] RecoveryCodes { get; set; }
+      if (RecoveryCodes == null || RecoveryCodes.Length == 0)
+      {
+        return RedirectToPage("./TwoFactorAuthentication");
+      }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [TempData]
-        public string StatusMessage { get; set; }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        public IActionResult OnGet()
-        {
-            if (RecoveryCodes == null || RecoveryCodes.Length == 0)
-            {
-                return RedirectToPage("./TwoFactorAuthentication");
-            }
-
-            return Page();
-        }
+      return Page();
     }
+  }
 }
