e4904663d75b14ed63ea6ec4685dcba5e533d502
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-20 12:49:22 +0200

    Add Identity tables to Db

--- YerbaMateStore/Migrations/20230420104732_AddIdentityTablesToDb.Designer.cs
+++ YerbaMateStore/Migrations/20230420104732_AddIdentityTablesToDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YerbaMateStore.Models.DataAccess;
 
@@ -10,9 +11,10 @@ using YerbaMateStore.Models.DataAccess;
 namespace YerbaMateStore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230420104732_AddIdentityTablesToDb")]
+    partial class AddIdentityTablesToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
