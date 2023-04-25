7b67a6933d68df2586d4c74fce20156a503208ee
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-04-25 10:21:57 +0200

    Identity: download all data - PersonalData attribute

--- YerbaMateStore/Models/Entities/ApplicationUser.cs
+++ YerbaMateStore/Models/Entities/ApplicationUser.cs
@@ -5,10 +5,16 @@ namespace YerbaMateStore.Models.Entities;
 public class ApplicationUser : IdentityUser
 {
   [Required]
+  [PersonalData]
   public string Name { get; set; }
+  [PersonalData]
   public string? StreetAddress { get; set; }
+  [PersonalData]
   public string? Country { get; set; }
+  [PersonalData]
   public string? City { get; set; }
+  [PersonalData]
   public string? State { get; set; }
+  [PersonalData]
   public string? PostalCode { get; set; }
 }
