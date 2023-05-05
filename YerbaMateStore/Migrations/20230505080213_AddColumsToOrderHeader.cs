f5dce80eb1fcd7a7330dd5a894d3f1e363b796cd
Tomasz Kedziorek <tomasz.kedziorek.programista@gmail.com>
2023-05-05 15:06:25 +0200

    Add colun to OrderHeader OrderAndDeliveryTotal

---
+++ YerbaMateStore/Migrations/20230505080213_AddColumsToOrderHeader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YerbaMateStore.Migrations
+{
+    public partial class AddColumsToOrderHeader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentDueDate",
+                table: "OrderHeader");
+
+            migrationBuilder.AddColumn<double>(
+                name: "OrderAndDeliveryTotal",
+                table: "OrderHeader",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentType",
+                table: "OrderHeader",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderAndDeliveryTotal",
+                table: "OrderHeader");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "OrderHeader");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentDueDate",
+                table: "OrderHeader",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
